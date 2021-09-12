90f1ecdbb82d42d1dbc0be9962aeb48fe3356f15
pabloferrari <pablo.ferrari@me.com>
2021-09-12 19:23:40 -0300

    UPD // Fixes

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,12 +1,25 @@
 import { Request, Response } from 'express';
 import { JWT_SECRET, JWT_EXPIRES_IN } from '../config/enviroments';
 import * as jwt from 'jsonwebtoken';
+import { logger } from '../lib/logger';
 
 export class AuthController {
 	
 	public static async validate(req: Request, res: Response): Promise<void> {
 		try {
-			res.status(200).json({ route: req.path });
+
+			let token = '';
+			if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
+				token = req.headers.authorization.split(' ')[1];
+			} else if (req.query && req.query.token) {
+				token = req.query.token as string;
+			}
+			
+			logger.info(`Token -> ${token}`);
+			const jwtPayload = jwt.verify(token, JWT_SECRET) as any;
+
+			res.status(200).json({ result: jwtPayload });
+			
 		} catch (error) {
 			res.status(500).json(error);
 		}
